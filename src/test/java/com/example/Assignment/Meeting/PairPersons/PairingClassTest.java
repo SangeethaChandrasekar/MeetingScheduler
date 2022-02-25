b6f7bcaf1bebe83294e13095c6d9542b895ba4ae
Sangeetha <sangeethachandrasekar911@gmail.com>
2022-02-25 18:26:04 +0530

    Initial Commit

--- src/test/java/com/example/Assignment/Meeting/PairPersons/PairingClassTest.java
+++ src/test/java/com/example/Assignment/Meeting/PairPersons/PairingClassTest.java
@@ -2,14 +2,15 @@ package com.example.Assignment.Meeting.PairPersons;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 import static org.junit.Assert.*;
 
 public class PairingClassTest {
-    ArrayList array = new ArrayList(Arrays.asList(1, 2, 3, 4));
-    ArrayList array1 = new ArrayList(Arrays.asList("1", "2"));
-    ArrayList array2 = new ArrayList(Arrays.asList(1.0, 2.25, 3.33));
-    ArrayList array3 = new ArrayList(Arrays.asList());
+    ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+    ArrayList<String> array1 = new ArrayList<>(Arrays.asList("1", "2"));
+    ArrayList<Double> array2 = new ArrayList<>(Arrays.asList(1.0, 2.25, 3.33));
+    ArrayList<Object> array3 = new ArrayList<>(Collections.emptyList());
 
 
     KWFour know4 = new KWFour(array3);
@@ -17,6 +18,7 @@ public class PairingClassTest {
 
     @org.junit.Test
     public void makePairingTest() {
+        assertEquals(know4.persons,array3);
         assertEquals((knowFour.makePairing(array)), array);
         assertEquals((knowFour.makePairing(array1)), array1);
         assertEquals((knowFour.makePairing(array2)), array2);
@@ -24,6 +26,7 @@ public class PairingClassTest {
     @org.junit.Test
     public void isEmptyTest() {
         assertTrue(knowFour.scheduleMeeting(array3));
+        assertEquals(know4.persons,array3);
     }
     @org.junit.Test
     public void scheduleMeetingTest() {
